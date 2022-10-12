7098bebc0d78d86777f92f4417910a37e51289a1
Maria Volintiru <maria.volintiru@yahoo.com>
2022-10-12 22:46:52 +0200

    Am introdus mesaj de bun venit dat utilizatorului Am introdus formula de salut  Am introdus parametru de tip intreg ale caror valori vor fi afisate utilizatorului impreuna cu un mesaj

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Volintiru_Andreea_Lab1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Ciao Maria");
+        }
+        public IActionResult Index2()
+        {
+            return Content("Bine ai venit");
+        }
+
+        public IActionResult Index3(string x,int n)
+        {
+            return Content("Valoarea generata pentru x este: negativa"+ x + ". Valoarea generata pentru n este: "+ n+ ".");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
